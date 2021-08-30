7c5ef64c02b23103671ed27ee890fad279570247
Tech Squid <techsquid@myhomeserver.com>
2021-08-30 18:39:17 -0400

    feat: start contact page

---
+++ pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="mainContainer">
+    <header class="text-center">
+      <div class="mb-8">
+        <p class="py-4"> My DMs 📭 are open ❤️</p>
+        <nuxt-link to="/follow" class="text-white rounded p-2 bg-blue-500 shadow-sm hover:bg-blue-400">DM me ✉️</nuxt-link>
+      </div>
+      <h1 class="blockedText text-6xl">Business Inqueries</h1>
+    </header>
+  </main>
+</template>
